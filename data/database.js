f130a0f45e03ee2281ecb4138c5e9c7c710f298b
Zainali005 <zainali5002@gmail.com>
2024-09-04 12:15:51 +0500

    update gitignore

--- data/database.js
+++ data/database.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = () => {
+    console.log("MongoDB URI:", process.env.MONGO_URI);
   mongoose
     .connect(process.env.MONGO_URI, {
       dbName: "FoodApp",
--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-import { connectDB } from "./data/database";
+import express from "express";
+import { config } from "dotenv";
+import { connectDB } from "./data/database.js";
 
-const express = require("express");
-const mongoose = require("mongoose");
 export const app = express();
 
 config({
   path: "./data/config.env",
 });
 connectDB();
-
 app.use(express.json());
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
